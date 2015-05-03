d45192d3377836083cb4fcad5d9b199a9e7003cd
Isan Sahoo <isansahoo@hotmail.com>
2015-05-03 12:14:50 -0700

    simple server socket - milestone-1

---
+++ server.py
@@ -0,0 +1,13 @@
+#simple server socket programming
+import socket
+
+#creating socket with IPv4 address family and TCP connection
+mySocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#server's IP and port is defined
+mySocket.bind(("localhost",8888))
+#sever can serve upto 3 client connections concurrently
+mySocket.listen(3)
+#blocking statement, waiting for client to connect
+(client,(ip,port))=mySocket.accept()
+
+
